1ce863c05afab760eca0aaee85a512bfcca3b928
Felix Kirsch <40427105+FelixKirsch@users.noreply.github.com>
2022-09-08 12:08:21 +0200

    Update Qdrant Version (#4)
    * changes for qdrant update, updates requirements
    
    * updates submodules/model
    
    * transfers neural search logic from app to util
    
    * adds session token
    
    * adds requirements for submodule model

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
-import os
-import numpy as np
 from fastapi import FastAPI, responses, status
-from qdrant_client import QdrantClient
-from neural_search.similarity_threshold import SimilarityThreshold
-from scipy.spatial.distance import cdist
-
-from submodules.model.business_objects import embedding
+from neural_search import util
+from submodules.model.business_objects import general
 
 app = FastAPI()
-port = int(os.environ["QDRANT_PORT"])
-qdrant_client = QdrantClient(host="qdrant", port=port)
-
-sim_thr = SimilarityThreshold(qdrant_client)
-
-missing_collections_creation_in_progress = False
 
 
 @app.get("/most_similar")
@@ -30,21 +19,9 @@ def most_similar(
     Returns:
         JSONResponse: containing HTML status code and the n most similar records
     """
-    embedding_item = embedding.get_tensor(embedding_id, record_id)
-    query_vector = np.array(embedding_item.data)
-    search_result = qdrant_client.search(
-        collection_name=embedding_id,
-        query_vector=query_vector,
-        query_filter=None,
-        top=limit,
-    )
-
-    # only return records which are more similar than the threshold
-    similarity_threshold = sim_thr.get_threshold(project_id, embedding_id)
-    similar_records = [
-        result.id for result in search_result if result.score >= similarity_threshold
-    ]
-
+    session_token = general.get_ctx_token()
+    similar_records = util.most_similar(project_id, embedding_id, record_id, limit)
+    general.remove_and_refresh_session(session_token)
     return responses.JSONResponse(
         status_code=status.HTTP_200_OK,
         content=[similar_records, ""],
@@ -52,7 +29,7 @@ def most_similar(
 
 
 @app.post("/recreate_collection")
-def recreate_collection(project_id: str, embedding_id: str) -> responses.JSONResponse:
+def recreate_collection(project_id: str, embedding_id: str) -> responses.HTMLResponse:
     """Create collection in Qdrant for the given embedding.
 
     Args:
@@ -60,25 +37,12 @@ def recreate_collection(project_id: str, embedding_id: str) -> responses.JSONRes
     Returns:
         JSONResponse: html status code
     """
-    tensors = embedding.get_tensors_by_embedding_id(embedding_id)
-    ids, embeddings = zip(*tensors)
-    embeddings = np.array(embeddings)
-
-    if len(embeddings) == 0:
+    session_token = general.get_ctx_token()
+    status_code = util.recreate_collection(project_id, embedding_id)
+    general.remove_and_refresh_session(session_token)
+    if status_code == 404:
         return responses.JSONResponse(status_code=status.HTTP_404_NOT_FOUND)
-
-    vector_size = embeddings.shape[-1]
-
-    qdrant_client.recreate_collection(
-        collection_name=embedding_id, vector_size=vector_size, distance="Euclid"
-    )
-    qdrant_client.upload_collection(
-        collection_name=embedding_id, vectors=embeddings, payload=None, ids=ids
-    )
-
-    sim_thr.calculate_threshold(project_id, embedding_id)
-
-    return responses.JSONResponse(status_code=status.HTTP_200_OK)
+    return responses.HTMLResponse(status_code=status.HTTP_200_OK)
 
 
 @app.get("/collections")
@@ -89,8 +53,7 @@ def get_collections() -> responses.JSONResponse:
     Returns:
         JSONResponse: html status code, list of collection names
     """
-    response = qdrant_client.openapi_client.collections_api.get_collections()
-    collections = [collection.name for collection in response.result.collections]
+    collections = util.get_collections()
     return responses.JSONResponse(status_code=status.HTTP_200_OK, content=collections)
 
 
@@ -102,49 +65,22 @@ def create_missing_collections() -> responses.JSONResponse:
     Returns:
         JSONResponse: html status code
     """
-    global missing_collections_creation_in_progress
-
-    if missing_collections_creation_in_progress:
+    session_token = general.get_ctx_token()
+    status_code, content = util.create_missing_collections()
+    general.remove_and_refresh_session(session_token)
+    if status_code == 429:
         return responses.JSONResponse(
-            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-            content="The missing colletions are already being created.",
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS, content=content
         )
-    missing_collections_creation_in_progress = True
-
-    response = qdrant_client.openapi_client.collections_api.get_collections()
-    collections = [collection.name for collection in response.result.collections]
-    embedding_items = embedding.get_finished_attribute_embeddings()
-
-    if not embedding_items:
-        missing_collections_creation_in_progress = False
+    elif status_code == 412:
         return responses.JSONResponse(
-            status_code=status.HTTP_412_PRECONDITION_FAILED,
-            content="There are no embeddings.",
+            status_code=status.HTTP_412_PRECONDITION_FAILED, content=content
         )
-
-    created_collections = []
-    for project_id, embedding_id in embedding_items:
-
-        if embedding_id in collections:
-            continue
-
-        try:
-            recreate_collection(project_id, embedding_id)
-            created_collections.append(embedding_id)
-        except Exception as e:
-            qdrant_client.delete_collection(embedding_id)
-            print(f"this did not work :(  -> {embedding_id}")
-            print(f"Aaaand the error goes to {e}")
-
-    missing_collections_creation_in_progress = False
-
-    return responses.JSONResponse(
-        status_code=status.HTTP_200_OK, content=created_collections
-    )
+    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=content)
 
 
 @app.put("/delete_collection")
-def delete_collection(embedding_id: str) -> responses.JSONResponse:
+def delete_collection(embedding_id: str) -> responses.HTMLResponse:
     """
     Delete collection in Qdrant for the given embedding.
 
@@ -153,8 +89,8 @@ def delete_collection(embedding_id: str) -> responses.JSONResponse:
     Returns:
         JSONResponse: html status code
     """
-    qdrant_client.delete_collection(embedding_id)
-    return responses.JSONResponse(status_code=status.HTTP_200_OK)
+    util.delete_collection(embedding_id)
+    return responses.HTMLResponse(status_code=status.HTTP_200_OK)
 
 
 @app.get("/detect_outliers")
@@ -171,38 +107,14 @@ def detect_outliers(
     Returns:
         JSONResponse: html status code, if successfull the response the top_n most outlying records.
     """
-    labeled_tensors = embedding.get_manually_labeled_tensors_by_embedding_id(
-        project_id, embedding_id
-    )
-    labeled_ids, labeled_embeddings = zip(*labeled_tensors)
-    labeled_embeddings = np.array(labeled_embeddings)
-
-    if len(labeled_ids) < 1:
+    session_token = general.get_ctx_token()
+    status_code, content = util.detect_outliers(project_id, embedding_id, limit)
+    general.remove_and_refresh_session(session_token)
+    if status_code == 412:
         return responses.JSONResponse(
-            status_code=status.HTTP_412_PRECONDITION_FAILED,
-            content="At least one record must be labeled manually to create outlier slice.",
+            status_code=status.HTTP_412_PRECONDITION_FAILED, content=content
         )
-
-    unlabeled_tensors = embedding.get_not_manually_labeled_tensors_by_embedding_id(
-        project_id, embedding_id
-    )
-    unlabeled_ids, unlabeled_embeddings = zip(*unlabeled_tensors)
-    unlabeled_embeddings = np.array(unlabeled_embeddings)
-
-    outlier_scores = np.sum(
-        cdist(labeled_embeddings, unlabeled_embeddings, "euclidean"), axis=0
-    )
-    sorted_index = np.argsort(
-        outlier_scores,
-        axis=None,
-    )[::-1]
-
-    max_records = min(round(0.05 * len(sorted_index)), limit)
-
-    outlier_ids = np.array(unlabeled_ids)[sorted_index[:max_records]]
-    outlier_scores = outlier_scores[sorted_index[:max_records]]
-
     return responses.JSONResponse(
         status_code=status.HTTP_200_OK,
-        content=[[outlier_ids.tolist(), outlier_scores.tolist()], ""],
+        content=[content, ""],
     )
---
+++ neural_search/util.py
@@ -0,0 +1,135 @@
+import numpy as np
+import os
+from qdrant_client import QdrantClient
+from scipy.spatial.distance import cdist
+from typing import Any, Tuple, Union, List
+
+from submodules.model.business_objects import embedding
+
+from .similarity_threshold import SimilarityThreshold
+
+port = int(os.environ["QDRANT_PORT"])
+qdrant_client = QdrantClient(host="qdrant", port=port)
+
+sim_thr = SimilarityThreshold(qdrant_client)
+
+missing_collections_creation_in_progress = False
+
+
+def most_similar(project_id: str, embedding_id: str, record_id: str, limit: int = 100):
+    embedding_item = embedding.get_tensor(embedding_id, record_id)
+    query_vector = np.array(embedding_item.data)
+    similarity_threshold = sim_thr.get_threshold(project_id, embedding_id)
+    search_result = qdrant_client.search(
+        collection_name=embedding_id,
+        query_vector=query_vector,
+        query_filter=None,
+        limit=limit,
+        score_threshold=similarity_threshold,
+    )
+    similar_records = [result.id for result in search_result]
+    return similar_records
+
+
+def recreate_collection(project_id: str, embedding_id: str) -> int:
+    tensors = embedding.get_tensors_by_embedding_id(embedding_id)
+    ids, embeddings = zip(*tensors)
+    embeddings = np.array(embeddings)
+
+    if len(embeddings) == 0:
+        return 404
+
+    vector_size = embeddings.shape[-1]
+
+    qdrant_client.recreate_collection(
+        collection_name=embedding_id, vector_size=vector_size, distance="Euclid"
+    )
+    qdrant_client.upload_collection(
+        collection_name=embedding_id, vectors=embeddings, payload=None, ids=ids
+    )
+
+    sim_thr.calculate_threshold(project_id, embedding_id)
+
+    return 200
+
+
+def get_collections():
+    response = qdrant_client.openapi_client.collections_api.get_collections()
+    collections = [collection.name for collection in response.result.collections]
+    return collections
+
+
+def create_missing_collections() -> Tuple[int, Union[List[str], str]]:
+    global missing_collections_creation_in_progress
+
+    if missing_collections_creation_in_progress:
+        return 429, "Another process is already creating missing collections."
+
+    missing_collections_creation_in_progress = True
+
+    response = qdrant_client.openapi_client.collections_api.get_collections()
+    collections = [collection.name for collection in response.result.collections]
+    embedding_items = embedding.get_finished_attribute_embeddings()
+
+    if not embedding_items:
+        missing_collections_creation_in_progress = False
+        return 412, "No embeddings found."
+
+    created_collections = []
+    for project_id, embedding_id in embedding_items:
+
+        if embedding_id in collections:
+            continue
+
+        try:
+            recreate_collection(project_id, embedding_id)
+            created_collections.append(embedding_id)
+        except Exception as e:
+            qdrant_client.delete_collection(embedding_id)
+            print(f"this did not work :(  -> {embedding_id}")
+            print(f"Aaaand the error goes to {e}")
+
+    missing_collections_creation_in_progress = False
+
+    return 200, created_collections
+
+
+def delete_collection(embedding_id: str):
+    qdrant_client.delete_collection(embedding_id)
+
+
+def detect_outliers(
+    project_id: str, embedding_id: str, limit: int = 100
+) -> Tuple[int, Union[List[Any], str]]:
+    labeled_tensors = embedding.get_manually_labeled_tensors_by_embedding_id(
+        project_id, embedding_id
+    )
+    labeled_ids, labeled_embeddings = zip(*labeled_tensors)
+    labeled_embeddings = np.array(labeled_embeddings)
+
+    if len(labeled_ids) < 1:
+        return (
+            412,
+            "At least one record must be labeled manually to create outlier slice.",
+        )
+
+    unlabeled_tensors = embedding.get_not_manually_labeled_tensors_by_embedding_id(
+        project_id, embedding_id
+    )
+    unlabeled_ids, unlabeled_embeddings = zip(*unlabeled_tensors)
+    unlabeled_embeddings = np.array(unlabeled_embeddings)
+
+    outlier_scores = np.sum(
+        cdist(labeled_embeddings, unlabeled_embeddings, "euclidean"), axis=0
+    )
+    sorted_index = np.argsort(
+        outlier_scores,
+        axis=None,
+    )[::-1]
+
+    max_records = min(round(0.05 * len(sorted_index)), limit)
+
+    outlier_ids = np.array(unlabeled_ids)[sorted_index[:max_records]]
+    outlier_scores = outlier_scores[sorted_index[:max_records]]
+
+    return 200, [outlier_ids.tolist(), outlier_scores.tolist()]
