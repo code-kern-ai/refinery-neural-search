e0368e18ae66b63681def36298f8b284e0a5e18a
Simon Degraf <125890757+SimonDegrafKern@users.noreply.github.com>
2023-03-09 11:57:11 +0100

    Healthcheck (#20)
    * Adds status code and removes headers
    
    * Replaces status codes

--- app.py
+++ app.py
@@ -136,8 +136,13 @@ def detect_outliers(
 
 @app.get("/healthcheck")
 def healthcheck() -> responses.PlainTextResponse:
-    headers = {"APP": "OK"}
+    text = ""
+    status_code = status.HTTP_200_OK
     database_test = general.test_database_connection()
     if not database_test.get("success"):
-        headers["DATABASE"] = database_test.get("error")
-    return responses.PlainTextResponse("OK", headers=headers)
+        error_name = database_test.get("error")
+        text += f"database_error:{error_name}:"
+        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    if not text:
+        text = "OK"
+    return responses.PlainTextResponse(text, status_code=status_code)
