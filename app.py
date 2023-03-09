334527c9ac7a22d22c8592f33032d4137130dc1f
SimonDegrafKern <simon.degraf@kern.ai>
2023-03-09 11:47:08 +0100

    Replaces status codes

--- app.py
+++ app.py
@@ -137,12 +137,12 @@ def detect_outliers(
 @app.get("/healthcheck")
 def healthcheck() -> responses.PlainTextResponse:
     text = ""
-    status_code = 200
+    status_code = status.HTTP_200_OK
     database_test = general.test_database_connection()
     if not database_test.get("success"):
         error_name = database_test.get("error")
         text += f"database_error:{error_name}:"
-        status_code = 500
+        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     if not text:
         text = "OK"
     return responses.PlainTextResponse(text, status_code=status_code)
